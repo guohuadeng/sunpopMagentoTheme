411a3a6d4b49127335aa474242b9ebcbf773cf9c
guohuadeng <guohuadeng@hotmail.com>
2016-01-23 23:08:15 +0800

    update translate ， add locoy

--- init_CN.sql
+++ init_CN.sql
@@ -1,16 +1,29 @@
 #安装1,SQL，修改基础信息
 UPDATE `core_config_data` SET `value`='0' WHERE `path`='general/region/display_all';
-UPDATE `core_config_data` SET `value`='http://aototech.sunpop.cn/' WHERE `path`='web/unsecure/base_url';
-UPDATE `core_config_data` SET `value`='http://aototech.sunpop.cn/' WHERE `path`='web/secure/base_url';
-UPDATE `core_config_data` SET `value`='en_US' WHERE `path`='general/locale/code';
+UPDATE `core_config_data` SET `value`='http://clean.sunpop.cn/' WHERE `path`='web/unsecure/base_url';
+UPDATE `core_config_data` SET `value`='http://clean.sunpop.cn/' WHERE `path`='web/secure/base_url';
+UPDATE `core_config_data` SET `value`='zh_CN' WHERE `path`='general/locale/code';
 UPDATE `core_config_data` SET `value`='Asia/Chongqing' WHERE `path`='general/locale/timezone';
 
-UPDATE `core_config_data` SET `value`='Aototech Online' WHERE `path`='general/store_information/name';
-UPDATE `core_config_data` SET `value`='Aototech Online' WHERE `path`='design/header/logo_alt';
-UPDATE `core_config_data` SET `value`='Aototech Online' WHERE `path`='design/head/default_title';
-UPDATE `core_config_data` SET `value`='Aototech Online' WHERE `path`='general/store_information/name';
-UPDATE `core_config_data` SET `value`='Welcome to Aototech' WHERE `path`='design/header/welcome';
-UPDATE `core_config_data` SET `value`='&copy; AotoTech.com. All rights reserved.' WHERE `path`='design/footer/copyright';
+UPDATE `core_config_data` SET `value`='CNY' WHERE `path`='currency/options/base';
+UPDATE `core_config_data` SET `value`='CNY' WHERE `path`='currency/options/default';
+UPDATE `core_config_data` SET `value`='CNY' WHERE `path`='currency/options/allow';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/country/default';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/country/allow';
+
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/region/state_required';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/country/default';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/country/allow';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/country/optional_zip_countries';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/store_information/merchant_countrys';
+UPDATE `core_config_data` SET `value`='CN' WHERE `path`='general/store_information/merchant_countrys';
+
+UPDATE `core_config_data` SET `value`='文宝印章' WHERE `path`='general/store_information/name';
+UPDATE `core_config_data` SET `value`='文宝印章' WHERE `path`='design/header/logo_alt';
+UPDATE `core_config_data` SET `value`='文宝印章' WHERE `path`='design/head/default_title';
+UPDATE `core_config_data` SET `value`='文宝印章' WHERE `path`='general/store_information/name';
+UPDATE `core_config_data` SET `value`='轻松定制建设执业资格印章！足不出户，直接快递上门' WHERE `path`='design/header/welcome';
+UPDATE `core_config_data` SET `value`='&copy; 版权所有 文宝印章' WHERE `path`='design/footer/copyright';
 
 UPDATE `core_config_data` SET `value`='1' WHERE `path`='admin/security/domain_policy_backend';
 UPDATE `core_config_data` SET `value`='1' WHERE `path`='admin/security/domain_policy_frontend';
@@ -20,6 +33,8 @@ UPDATE `core_config_data` SET `value`='1' WHERE `path`='web/seo/use_rewrites';
 
 UPDATE `core_config_data` SET `value`='1' WHERE `path`='catalog/custom_options/use_calendar';
 
+UPDATE `poll` SET `poll_title`='您最喜欢什么颜色？' WHERE `poll_title`='What is your favorite color';
+UPDATE `core_store` SET `code`='cn',`name`='中文' WHERE `code`='default';
 DELETE FROM  `adminnotification_inbox`;
 
 #禁用一些模块
@@ -41,9 +56,12 @@ INSERT INTO `core_config_data` (`scope`, `scope_id`, `path`, `value`) VALUES
 ('default', 0, 'advanced/modules_disable_output/Mage_Newsletter', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Oauth', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Paygate', '1'),
+('default', 0, 'advanced/modules_disable_output/Mage_Paypal', '1'),
+('default', 0, 'advanced/modules_disable_output/Mage_PaypalUk', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Rating', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Review', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Rss', '1'),
+('default', 0, 'advanced/modules_disable_output/Mage_Sendfriend', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Sitemap', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Tag', '1'),
 ('default', 0, 'advanced/modules_disable_output/Mage_Usa', '1'),
